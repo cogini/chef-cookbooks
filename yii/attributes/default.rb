2ea58e325dded7b7d91db8e894728a565d5af98f
Hoang Xuan Phu <phunehehe@gmail.com>
2012-07-12 15:49:12 +0700

    yii cookbook

---
+++ yii/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:yii][:version] = "1.1.10"
+default[:yii][:symlink] = nil
--- yii/recipes/default.rb
+++ yii/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+require_recipe "git"
+
+bash "install-yii" do
+    cwd "/opt"
+    code <<-EOH
+        git clone https://github.com/yiisoft/yii.git
+        cd yii
+        git fetch
+        git checkout #{node[:yii][:version]}
+    EOH
+end
+
+execute "ln -s /opt/yii #{node[:yii][:symlink]}" do
+    only_if {node[:yii][:symlink]}
+end
