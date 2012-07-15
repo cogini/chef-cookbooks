cee231e3285034d0fa1957c85f6f7c0254b80f7b
Hoang Xuan Phu <phunehehe@gmail.com>
2012-07-15 17:55:50 +0000

    add the moodle cookbook

---
+++ moodle/attributes/default.rb
@@ -0,0 +1,6 @@
+default[:moodle][:site_dir] = "/var/www/moodle"
+default[:moodle][:data_dir] = "/var/moodledata"
+default[:moodle][:user] = "www-data"
+default[:moodle][:group] = "www-data"
+default[:moodle][:branch] = "MOODLE_23_STABLE"
+default[:moodle][:cron_time] = 7
---
+++ moodle/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: moodle
+# Recipe:: default
+#
+# Copyright 2012, Cogini
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "git"
+include_recipe "php::module_soap"
+include_recipe "php::module_dom"
+include_recipe "php::module_mbstring"
+include_recipe "php::module_xmlrpc"
+include_recipe "php::module_gd"
+include_recipe "php::module_intl"
+
+
+site_dir = node.moodle.site_dir
+data_dir = node.moodle.data_dir
+moodle_user = node.moodle.user
+moodle_group = node.moodle.group
+
+
+[site_dir, data_dir].each do |dir|
+    directory dir do
+        action :create
+        recursive true
+        group moodle_group
+    end
+end
+
+bash "install moodle from git" do
+    code <<-EOH
+        git clone git://git.moodle.org/moodle.git #{site_dir}
+        cd #{site_dir}
+        git fetch
+        git checkout #{node[:moodle][:branch]}
+        chown -R #{moodle_user}:#{moodle_group} #{data_dir}
+    EOH
+end
+
+
+cron "moodle maintenance cron" do
+    hour node[:moodle][:cron_time]
+    minute "0"
+    command "php #{site_dir}/admin/cli/cron.php"
+end
