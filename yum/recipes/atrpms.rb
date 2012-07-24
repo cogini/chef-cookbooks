9238af55a47342a00a39d4dd8e7a8b060379f224
Hoang Xuan Phu <phu@cogini.com>
2012-07-24 01:13:10 -0400

    easy atrpms

--- yum/recipes/atrpms.rb
+++ yum/recipes/atrpms.rb
@@ -7,57 +7,39 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package_name = "atrpms-repo"
+package_name = 'atrpms-repo'
 
-if node.platform_version.to_f >= 6 and node.platform_version.to_f < 7
-    execute "atrpms el6 64bit" do
-        #64bit system
-        command "rpm -i http://dl.atrpms.net/all/#{package_name}-6-5.el6.x86_64.rpm"
-        only_if "
-            if uname -m | grep 64 && (if rpm -q #{package_name}; then exit 1; else exit 0; fi)
-            then exit 0
-            else exit 1
-            fi
-        "
-    end
-
-    execute "atrpms el6 32bit" do
-        #32bit system
-        command "rpm -i http://dl.atrpms.net/all/#{package_name}-6-5.el6.i686.rpm"
-        only_if "
-            if uname -m | grep-v 64 && (if rpm -q #{package_name}; then exit 1; else exit 0; fi)
-            then exit 0
-            else exit 1
-            fi
-        "
-    end
-
-elsif node.platform_version.to_f >= 5 and node.platform_version.to_f < 6
-    execute "atrpms el5 64bit" do
-        #64bit system
-        command "rpm -i http://dl.atrpms.net/all/#{package_name}-5-5.el5.x86_64.rpm"
-        only_if "
-            if uname -m | grep 64 && (if rpm -q #{package_name}; then exit 1; else exit 0; fi)
-            then exit 0
-            else exit 1
-            fi
-        "
-    end
-
-    execute "atrpms el5 32bit" do
-        #32bit system
-        command "rpm -i http://dl.atrpms.net/all/#{package_name}-5-5.el5.i386.rpm"
-        only_if "
-            if uname -m | grep -v 64 && (if rpm -q #{package_name}; then exit 1; else exit 0; fi)
-            then exit 0
-            else exit 1
-            fi
-        "
-    end
+package package_name do
+    action :remove
 end
 
+key = "#{node.platform}#{node.platform_version.to_i}-#{`uname -m | grep 64` ? '64' : '32'}"
 
-template "/etc/yum.repos.d/atrpms-testing.repo" do
-    mode "0644"
-    source "atrpms-testing.repo"
+case key
+when "centos6-64"
+    url = 'http://dl.atrpms.net/all/atrpms-repo-6-5.el6.x86_64.rpm'
+when "centos6-32"
+    url = 'http://dl.atrpms.net/all/atrpms-repo-6-5.el6.i686.rpm'
+when "centos5-64"
+    url = 'http://dl.atrpms.net/all/atrpms-repo-5-5.el5.x86_64.rpm'
+when "centos5-32"
+    url = 'http://dl.atrpms.net/all/atrpms-repo-5-5.el5.i386.rpm'
+else
+    raise NotImplementedError
 end
+
+rpm_file = "#{Chef::Config[:file_cache_path]}/#{File.basename(url)}"
+
+remote_file rpm_file do
+    source url
+    action :create_if_missing
+end
+
+execute "install atrpms" do
+    command "yum install -y #{rpm_file}"
+end
+
+#template "/etc/yum.repos.d/atrpms-testing.repo" do
+#    mode "0644"
+#    source "atrpms-testing.repo"
+#end
