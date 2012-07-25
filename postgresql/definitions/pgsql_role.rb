27e3ef3a4dfb29396f8cb08f01256f065702f352
Hoang Xuan Phu <phunehehe@gmail.com>
2012-07-25 16:20:21 +0700

    a recipe for role

---
+++ postgresql/definitions/pgsql_role.rb
@@ -0,0 +1,13 @@
+define :pgsql_role, :action => :create, :options => [] do
+
+    role_name = params[:name]
+    options = params[:options]
+
+    bash "create user #{role_name}" do
+        user "postgres"
+        code <<-EOH
+            psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='#{role_name}'" | grep -q 1 ||
+            psql postgres -tAc "CREATE ROLE #{role_name} #{options.join(' ')}"
+        EOH
+    end
+end
