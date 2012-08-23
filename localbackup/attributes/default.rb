6f6db68cb819246f39b757f7c3424affabe9faa8
Hoang Xuan Phu <phu@cogini.com>
2012-08-23 09:37:18 +0000

    auto detect mysql and backup

--- localbackup/attributes/default.rb
+++ localbackup/attributes/default.rb
@@ -3,5 +3,7 @@ default[:localbackup][:dirs] = %w{
     /etc
 }
 default[:localbackup][:time] = 17
-default[:localbackup][:mysql][:enable] = false
+default[:localbackup][:mysql][:enable] = File.exists?('/usr/bin/mysqldump')
+default[:localbackup][:mysql][:user] = 'root'
+default[:localbackup][:mysql][:password] = node[:mysql][:server_root_password]
 default[:localbackup][:pgsql][:enable] = File.exists?('/usr/bin/pg_dump')
