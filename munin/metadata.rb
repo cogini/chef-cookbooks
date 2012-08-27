05b2946486ef573e7d3482c56ea53a3096db1414
Hoang Xuan Phu <phunehehe@gmail.com>
2012-08-27 15:16:41 +0700

    names

--- munin/metadata.rb
+++ munin/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Cogini"
+maintainer_email "phu@cogini.com"
 license          "All rights reserved"
 description      "Installs/Configures munin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- munin/recipes/default.rb
+++ munin/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: munin
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Cogini
 #
 # All rights reserved - Do Not Redistribute
 #
