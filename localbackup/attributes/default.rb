51086aa8e8e9cd887c1f49f674e62ae18377707f
Hoang Xuan Phu <phu@cogini.com>
2012-08-28 06:56:44 -0400

    smarter in mysql backup

--- localbackup/attributes/default.rb
+++ localbackup/attributes/default.rb
@@ -3,7 +3,10 @@ default[:localbackup][:dirs] = %w{
     /etc
 }
 default[:localbackup][:time] = 17
-default[:localbackup][:mysql][:enable] = File.exists?('/usr/bin/mysqldump')
-default[:localbackup][:mysql][:user] = 'root'
-default[:localbackup][:mysql][:password] = node[:mysql][:server_root_password]
+
+if (default[:localbackup][:mysql][:enable] = File.exists?('/usr/bin/mysqldump'))
+    default[:localbackup][:mysql][:user] = 'root'
+    default[:localbackup][:mysql][:password] = node[:mysql][:server_root_password]
+end
+
 default[:localbackup][:pgsql][:enable] = File.exists?('/usr/bin/pg_dump')
