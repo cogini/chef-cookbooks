f7c884d3096d48733096808edaefe6326cd740aa
Hoang Xuan Phu <phunehehe@gmail.com>
2012-08-28 11:52:09 +0700

    some instructions for hardy

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -6,3 +6,8 @@ gpg --export packages@opscode.com > /etc/apt/trusted.gpg.d/opscode-keyring.gpg
 apt-get update
 apt-get install opscode-keyring
 echo "chef chef/chef_server_url string none" | debconf-set-selections && apt-get install chef -y
+
+# Hardy is funny
+# Add this ppa https://launchpad.net/~ubuntu-ruby/+archive/ppa
+#sudo apt-get install build-essential libopenssl-ruby ruby1.8-dev rubygems
+#sudo gem install chef
