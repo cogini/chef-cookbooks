629eb43562b4031b4f47dffb469e457305627310
Hoang Xuan Phu <phunehehe@gmail.com>
2012-11-22 09:22:41 +0000

    sort keys in postfix config

--- postfix/attributes/default.rb
+++ postfix/attributes/default.rb
@@ -15,27 +15,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default[:postfix][:aliases] = {}
-default[:postfix][:inet_protocols] = 'all'
-default[:postfix][:mail_relay_networks] = ['127.0.0.0/8']
-default[:postfix][:mail_type] = 'client'
-default[:postfix][:multi_environment_relay] = false
-default[:postfix][:mydomain] = node[:domain]
-default[:postfix][:myhostname] = node[:fqdn]
-default[:postfix][:myorigin] = '$myhostname'
-default[:postfix][:relayhost] = ''
-default[:postfix][:relayhost_role] = 'relayhost'
-default[:postfix][:smtp_sasl_auth_enable] = 'no'
-default[:postfix][:smtp_sasl_passwd] = ''
-default[:postfix][:smtp_sasl_password_maps] = 'hash:/etc/postfix/sasl_passwd'
-default[:postfix][:smtp_sasl_security_options] = 'noanonymous'
-default[:postfix][:smtp_sasl_user_name] = ''
-default[:postfix][:smtp_tls_cafile] = '/etc/postfix/cacert.pem'
-default[:postfix][:smtp_use_tls] = 'yes'
-default[:postfix][:smtpd_use_tls] = 'yes'
-default[:postfix][:disable_dns_lookups] = 'no'
-default[:postfix][:relay_domains] = [:$mydestination]
-default[:postfix][:relay_recipient_maps] = ''
-default[:postfix][:smtpd_recipient_restrictions] = %w{ permit_mynetworks reject_unauth_destination }
-default[:postfix][:transport_maps] = {}
-default[:postfix][:transport_maps_file] = 'hash:/etc/postfix/transport'
+default[:postfix] = {
+    :aliases = {}
+    :disable_dns_lookups = 'no'
+    :inet_protocols = 'all'
+    :mail_relay_networks = ['127.0.0.0/8']
+    :mail_type = 'client'
+    :multi_environment_relay = false
+    :mydomain = node[:domain]
+    :myhostname = node[:fqdn]
+    :myorigin = '$myhostname'
+    :relay_domains = [:$mydestination]
+    :relay_recipient_maps = ''
+    :relayhost = ''
+    :relayhost_role = 'relayhost'
+    :smtp_sasl_auth_enable = 'no'
+    :smtp_sasl_passwd = ''
+    :smtp_sasl_password_maps = 'hash:/etc/postfix/sasl_passwd'
+    :smtp_sasl_security_options = 'noanonymous'
+    :smtp_sasl_user_name = ''
+    :smtp_tls_cafile = '/etc/postfix/cacert.pem'
+    :smtp_use_tls = 'yes'
+    :smtpd_recipient_restrictions = %w{ permit_mynetworks reject_unauth_destination }
+    :smtpd_use_tls = 'yes'
+    :transport_maps = {}
+    :transport_maps_file = 'hash:/etc/postfix/transport'
+}
