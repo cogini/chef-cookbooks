be47524005c2a7cd77dcc10196c2cb2db6c532e3
Hoang Xuan Phu <phunehehe@gmail.com>
2012-11-27 16:07:03 +0000

    add a definition for yii

---
+++ yii/definitions/yii_framework.rb
@@ -0,0 +1,19 @@
+define :yii_framework do
+
+    yii_version = params[:name]
+    yii_path = params[:path] || "/opt/yii-#{yii_version}"
+
+    directory File.dirname(yii_path) do
+        action :create
+        recursive true
+    end
+
+    bash 'install-yii' do
+        code <<-EOH
+            [[ -d #{yii_path} ]] || git clone https://github.com/yiisoft/yii.git #{yii_path}
+            cd #{yii_path}
+            git fetch
+            git checkout #{yii_version}
+        EOH
+    end
+end
