b2df23b9874b64a9efdc293eac363f7414ef864f
Hoang Xuan Phu <phunehehe@gmail.com>
2012-12-10 02:30:06 -0700

    tweaks for hardy

--- fpm/attributes/default.rb
+++ fpm/attributes/default.rb
@@ -8,9 +8,14 @@ when "redhat", "centos"
     set[:fpm][:provider] = Chef::Provider::Package::Rpm
 else
     # Ubuntu
-    set[:fpm][:dependencies] = %w{
-        rubygems
-        ruby-dev
-    }
     set[:fpm][:provider] = Chef::Provider::Package::Dpkg
+    if node[:platform_version].to_f >= 10.04
+        set[:fpm][:dependencies] = %w{
+            rubygems
+            ruby-dev
+        }
+    else
+        set[:fpm][:dependencies] = %w{
+        }
+    end
 end
