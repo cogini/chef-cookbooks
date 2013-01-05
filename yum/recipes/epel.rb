1312d66da6f73e41e58590b19a7a52d22bf53753
Hoang Xuan Phu <phunehehe@gmail.com>
2013-01-05 11:20:59 +0700

    epel update their repo

--- yum/recipes/epel.rb
+++ yum/recipes/epel.rb
@@ -11,7 +11,7 @@ key = "#{node.platform}#{node.platform_version.to_i}"
 
 case key
 when 'centos6', 'amazon2012'
-    url = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm'
+    url = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
 when 'centos5'
     url = 'http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm'
 else
