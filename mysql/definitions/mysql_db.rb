b0e798a210d6d58c2fc593aa8e2921edbb82a7d5
Hoang Xuan Phu <phunehehe@gmail.com>
2013-01-11 07:03:08 +0000

    who would not want utf8?

--- mysql/definitions/mysql_db.rb
+++ mysql/definitions/mysql_db.rb
@@ -9,6 +9,7 @@ define :mysql_db, :action => :create, :owner => 'root', :host => 'localhost' do
         code <<-EOH
 mysql -uroot -p#{root_password} <<"EOF"
     CREATE DATABASE IF NOT EXISTS `#{db_name}`;
+    ALTER DATABASE #{db_name} CHARACTER SET utf8;
     GRANT ALL ON `#{db_name}`.* TO '#{owner}'@'#{host}';
     FLUSH PRIVILEGES;
 EOF
