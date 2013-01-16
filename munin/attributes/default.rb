7f2d8e3599c5ae3f8e32f6f84198c29f293dfb64
Hoang Xuan Phu <phunehehe@gmail.com>
2013-01-16 12:14:46 +0000

    fixup for hardy

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -2,6 +2,10 @@ case node[:platform]
 when 'centos', 'redhat', 'fedora', 'amazon'
     default[:munin][:packages] = %w{ munin-node }
 when 'debian', 'ubuntu'
-    default[:munin][:packages] = %w{ munin-node munin-plugins-extra}
+    if node[:platform_version].to_f >= 10.04
+        default[:munin][:packages] = %w{ munin-node munin-plugins-extra }
+    else
+        default[:munin][:packages] = %w{ munin-node }
+    end
 end
 
