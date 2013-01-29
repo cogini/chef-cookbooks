694ca744c601f160f25c3d791ea3dd1ba5e4152c
Hoang Xuan Phu <phunehehe@gmail.com>
2013-01-28 23:50:37 -0500

    attribute for log location

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,7 +1,9 @@
 case node[:platform]
 when 'centos', 'redhat', 'fedora', 'amazon'
     default[:munin][:packages] = %w{ munin-node }
+    set[:munin][:log_dir] = '/var/log/munin-node'
 when 'debian', 'ubuntu'
+    set[:munin][:log_dir] = '/var/log/munin'
     if node[:platform_version].to_f >= 10.04
         default[:munin][:packages] = %w{ munin-node munin-plugins-extra }
     else
