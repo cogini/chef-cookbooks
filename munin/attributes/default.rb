58cb38d5831a55030b72f5a7dba822a96d79fd4e
Hoang Xuan Phu <phunehehe@gmail.com>
2013-01-28 23:51:17 -0500

    set instead of default

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,13 +1,13 @@
 case node[:platform]
 when 'centos', 'redhat', 'fedora', 'amazon'
-    default[:munin][:packages] = %w{ munin-node }
+    set[:munin][:packages] = %w{ munin-node }
     set[:munin][:log_dir] = '/var/log/munin-node'
 when 'debian', 'ubuntu'
     set[:munin][:log_dir] = '/var/log/munin'
     if node[:platform_version].to_f >= 10.04
-        default[:munin][:packages] = %w{ munin-node munin-plugins-extra }
+        set[:munin][:packages] = %w{ munin-node munin-plugins-extra }
     else
-        default[:munin][:packages] = %w{ munin-node }
+        set[:munin][:packages] = %w{ munin-node }
     end
 end
 
