6bac67a54f2e149b0c826f7a273c6d6ce3ca033b
Nhan Nguyen <ducnhan813@gmail.com>
2013-02-08 10:08:06 +0700

    Edit attributes

--- gitlab/attributes/default.rb
+++ gitlab/attributes/default.rb
@@ -1,13 +1,13 @@
 # Required dependencies
-default[:gitlab][:dependencies] = %w{ sudo build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev ruby1.9.3 python vim nginx }
+set[:gitlab][:dependencies] = %w{ sudo build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev ruby1.9.3 }
 
 # Users info
-default[:user][:git][:name] = "git"
-default[:user][:git][:shell] = "/bin/bash"
-default[:user][:git][:home] = "/home/git"
+default[:gitlab][:git_user][:name] = "git"
+default[:gitlab][:git_user][:shell] = "/bin/bash"
+default[:gitlab][:git_user][:home] = "/home/git"
 
-default[:user][:gitlab][:name] = "gitlab"
-default[:user][:gitlab][:shell] = "/bin/bash"
-default[:user][:gitlab][:home] = "/home/gitlab"
+default[:gitlab][:gitlab_user][:name] = "gitlab"
+default[:gitlab][:gitlab_user][:shell] = "/bin/bash"
+default[:gitlab][:gitlab_user][:home] = "/home/gitlab"
 
-default[:user][:group] = "git"
+default[:gitlab][:group] = "git"
