f2beb59c9c3c6813391417dfac206a49b4b2aef2
Hoang Xuan Phu <phunehehe@gmail.com>
2013-02-18 10:54:54 +0000

    use install script from veewee

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# For Hardy see http://stackoverflow.com/questions/1814301/updating-rubygems-on-ubuntu
+# Taken from https://github.com/jedi4ever/veewee/blob/master/templates/ubuntu-12.04.2-server-i386/postinstall.sh
 
-sudo apt-get update
-sudo apt-get install build-essential libopenssl-ruby ruby1.8-dev rubygems
-sudo gem install chef
+# Install Ruby from source in /opt so that users of Vagrant
+# can install their own Rubies using packages or however.
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
+tar xvzf ruby-1.9.2-p290.tar.gz
+cd ruby-1.9.2-p290
+./configure --prefix=/opt/ruby
+make
+make install
+cd ..
+rm -rf ruby-1.9.2-p290
 
-for i in $(echo chef-solo knife)
-do
-    sudo ln -s /var/lib/gems/1.8/bin/$i /usr/bin
-done
+# Install RubyGems 1.7.2
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.11.tgz
+tar xzf rubygems-1.8.11.tgz
+cd rubygems-1.8.11
+/opt/ruby/bin/ruby setup.rb
+cd ..
+rm -rf rubygems-1.8.11
+
+# Installing chef & Puppet
+/opt/ruby/bin/gem install chef --no-ri --no-rdoc
+#/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
+
+# Add /opt/ruby/bin to the global path as the last resort so
+# Ruby, RubyGems, and Chef/Puppet are visible
+echo 'PATH=$PATH:/opt/ruby/bin/'> /etc/profile.d/vagrantruby.sh
