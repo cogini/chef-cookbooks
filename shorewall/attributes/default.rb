a7b8925a990e50451d5de8bcc81daf3f40850919
Nhan Nguyen <ducnhan813@gmail.com>
2013-02-28 17:54:54 +0700

    Edited structure for allowing host A connect to Port B

--- shorewall/attributes/default.rb
+++ shorewall/attributes/default.rb
@@ -8,7 +8,7 @@ default[:shorewall][:allowed_hosts] = []
 
 # Allow connections from A:any to server:B
 # [
-#   ["source ip", "dest port"],
+#   ["IP1 IP2 ...", "PortA PortB"],
 #   ...
 # ]
-default[:shorewall][:allowed_connections] = []
+default[:shorewall][:allowed_connections] = [["172.16.1.1 172.16.1.2","23 1000 1001 1002"]]
