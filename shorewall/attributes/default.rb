884c106dbcda977420aaced2666df6c1dbbcafa5
Nhan Nguyen <ducnhan813@gmail.com>
2013-02-28 18:05:06 +0700

    Removed uneeded code

--- shorewall/attributes/default.rb
+++ shorewall/attributes/default.rb
@@ -8,7 +8,7 @@ default[:shorewall][:allowed_hosts] = []
 
 # Allow connections from A:any to server:B
 # [
-#   ["IP1 IP2 ...", "PortA PortB"],
+#   ["IP1,IP2,...", "PortA,PortB,"],
 #   ...
 # ]
 default[:shorewall][:allowed_connections] = []
