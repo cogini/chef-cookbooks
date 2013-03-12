9a5b8c6895326499cbab8773c91551b46f55a3b7
Nhan Nguyen <ducnhan813@gmail.com>
2013-03-12 15:25:58 +0700

    Shorewall: accept connections from host A to port B of server

--- shorewall/attributes/default.rb
+++ shorewall/attributes/default.rb
@@ -5,3 +5,9 @@ default[:shorewall][:allowed_ports] = [
 ]
 default[:shorewall][:blocked_ports] = []
 default[:shorewall][:allowed_hosts] = []
+
+# default[:shorewall][:allowed_connections] = [
+#                                               ["192.168.1.1,192.168.1.2","1000,1001,1002"],
+#                                               ["192.168.1.3","1004"]
+#                                             ]
+default[:shorewall][:allowed_connections] = []
