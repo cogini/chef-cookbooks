c422760581d3324bdc9af3ab208dc73d6364504a
Hoang Xuan Phu <phunehehe@gmail.com>
2013-03-26 01:57:43 -0400

    dependencies

--- nagios/metadata.rb
+++ nagios/metadata.rb
@@ -4,5 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures nagios"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-depends          "openssl"
 depends          "logwarn"
+depends          "mysql"
+depends          "openssl"
+depends          "postgresql"
