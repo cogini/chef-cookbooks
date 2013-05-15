12d15404ae9a492a8fe562f0c3e26927c4bf689c
Hoang Xuan Phu <phunehehe@gmail.com>
2013-05-15 10:15:48 +0000

    separate config, run and prune

--- tarsnap/files/default/tarsnap-env.sh
+++ tarsnap/files/default/tarsnap-env.sh
@@ -13,3 +13,32 @@ read_key="$tarsnapdir/$(hostname -s)-tarsnap.key"
 cachedir="$tarsnapdir/cachedir"
 
 tarsnap="/usr/local/bin/tarsnap --cachedir $cachedir"
+
+
+# Directories to backup
+DIRS="<%= node[:tarsnap][:dirs].join(' ') %>"
+
+# Number of daily backups to keep
+DAILY=7
+
+# Number of weekly backups to keep
+WEEKLY=4
+# Which day to do weekly backups on
+# 1-7, Monday = 1
+WEEKLY_DAY=1
+
+# Number of monthly backups to keep
+MONTHLY=3
+# Which day to do monthly backups on
+# 01-31 (leading 0 is important)
+MONTHLY_DAY=01
+
+# Path to tarsnap
+#TARSNAP="/home/tdb/tarsnap/tarsnap.pl"
+TARSNAP="/usr/local/bin/tarsnap"
+
+DIR="$(cd "$(dirname "$0")"; pwd)"
+KEY="$DIR/$(hostname -s)-tarsnap.key"
+CACHE_DIR="$DIR/cachedir"
+
+# end of config
--- tarsnap/files/default/tarsnap-prune.sh
+++ tarsnap/files/default/tarsnap-prune.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
-
 # Remove old backups
 
-# Number of backups to keep
-backup_count=10
 
-set -e
 . "$(dirname "$0")/tarsnap-env.sh"
 
-archives="$("$tarsnapdir/tarsnap-list.sh")"
-first_date="$(date --date="$backup_count days ago" +'%Y-%m-%d')"
 
-echo "$archives" | while read archive
-do
-    name="$(echo "$archive" | cut -f1)"
-    date="$(echo "$archive" | cut -f2)"
-    if [[ "$date" < "$first_date" ]]
-    then
-        echo "Deleting $name"
-        $tarsnap --keyfile "$read_key" -d -f "$name"
-    fi
+# Using tail to find archives to delete, but its
+# +n syntax is out by one from what we want to do
+# (also +0 == +1, so we're safe :-)
+DAILY=`expr $DAILY + 1`
+WEEKLY=`expr $WEEKLY + 1`
+MONTHLY=`expr $MONTHLY + 1`
+DATETIME_REGEX="[[:digit:]]{8}-[[:digit:]]{6}"
+
+
+TMPFILE=/tmp/tarsnap.archives.$$
+$TARSNAP --list-archives > $TMPFILE
+
+
+# Do deletes
+for dir in $DIRS; do
+    for i in `grep -E "^$DATETIME_REGEX-daily-$dir" $TMPFILE | sort -rn | tail -n +$DAILY`; do
+        echo "==> delete $i"
+        $TARSNAP -d -f $i
+    done
+    for i in `grep -E "^$DATETIME_REGEX-weekly-$dir" $TMPFILE | sort -rn | tail -n +$WEEKLY`; do
+        echo "==> delete $i"
+        $TARSNAP -d -f $i
+    done
+    for i in `grep -E "^$DATETIME_REGEX-monthly-$dir" $TMPFILE | sort -rn | tail -n +$MONTHLY`; do
+        echo "==> delete $i"
+        $TARSNAP -d -f $i
+    done
 done
+
+
+# Look for unrecognized backups
+
+echoerr() {
+    echo "$@" 1>&2
+}
+
+WEIRD=$(grep -Ev "^$DATETIME_REGEX-(daily|weekly|monthly)-(<%= node[:tarsnap][:dirs].join('|') %>)" $TMPFILE)
+if [ -n "$WEIRD" ]
+then
+    echoerr "Unrecognized backups found:"
+    for i in $WEIRD
+    do
+        echoerr " - $i" >&2
+    done
+fi
+
+
+rm $TMPFILE
