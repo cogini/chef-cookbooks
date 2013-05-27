d98018f1a0fde969262f74c3e8157ee7f3433455
Hoang Xuan Phu <phunehehe@gmail.com>
2013-05-27 05:15:44 +0000

    stupid mysql...

--- localbackup/attributes/default.rb
+++ localbackup/attributes/default.rb
@@ -4,10 +4,15 @@ set[:localbackup][:ignore_file] = "#{script_dir}/ignore"
 set[:localbackup][:script_file] = "#{script_dir}/make_backup.sh"
 
 default[:localbackup][:destination] = '/usr/local/backups'
+default[:localbackup][:dirs] = %w{ /etc }
 default[:localbackup][:ignore] = []
 
 default[:localbackup][:mysql][:enable] = File.exists?('/etc/init.d/mysql')
 default[:localbackup][:pgsql][:enable] = (not Dir.glob('/etc/init.d/postgresql*').empty?)
+
+# MySQL 5.1 an up needs `--events`
+# MySQL 5.0 and down doesn't have it
+default[:localbackup][:mysql][:extra_options] = '--events'
 
 case node[:platform]
 when 'ubuntu'
@@ -15,8 +20,7 @@ when 'ubuntu'
         /etc
         /var/backups
     }
-else
-    default[:localbackup][:dirs] = %w{
-        /etc
-    }
+    if node[:platform_version].to_f < 10.04
+        default[:localbackup][:mysql][:extra_options] = ''
+    end
 end
