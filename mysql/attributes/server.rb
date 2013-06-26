f034b2a2f7bf30215d43622a6e23b001e8e90282
Hoang Xuan Phu <phunehehe@gmail.com>
2013-06-26 11:36:36 +0000

    update default attributes

--- mysql/attributes/server.rb
+++ mysql/attributes/server.rb
@@ -96,12 +96,12 @@ default['mysql']['allow_remote_root']               = false
 default['mysql']['tunable']['back_log']             = "128"
 default['mysql']['tunable']['key_buffer']           = "256M"
 default['mysql']['tunable']['max_allowed_packet']   = "16M"
-default['mysql']['tunable']['max_connections']      = "800"
+default['mysql']['tunable']['max_connections']      = 151
 default['mysql']['tunable']['max_heap_table_size']  = "32M"
 default['mysql']['tunable']['myisam_recover']       = "BACKUP"
 default['mysql']['tunable']['net_read_timeout']     = "30"
 default['mysql']['tunable']['net_write_timeout']    = "30"
-default['mysql']['tunable']['table_open_cache']     = "128"
+default['mysql']['tunable']['table_open_cache']     = 64
 default['mysql']['tunable']['thread_cache']         = "128"
 default['mysql']['tunable']['thread_cache_size']    = 8
 default['mysql']['tunable']['thread_concurrency']   = 10
@@ -117,4 +117,7 @@ default['mysql']['tunable']['long_query_time']      = 2
 default['mysql']['tunable']['expire_logs_days']     = 10
 default['mysql']['tunable']['max_binlog_size']      = "100M"
 
-default['mysql']['tunable']['innodb_buffer_pool_size']  = "256M"
+default['mysql']['tunable']['innodb_buffer_pool_size']  = "128M"
+
+default[:mysql][:tunable][:interactive_timeout] = 300
+default[:mysql][:tunable][:join_buffer_size] = "128K"
