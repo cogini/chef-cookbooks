6ef7b1385827ad10677f6a0f491edade5f9f8aca
Hoang Xuan Phu <phunehehe@gmail.com>
2013-07-09 06:32:18 +0000

    fix localbackup for old versions of tar

--- localbackup/attributes/default.rb
+++ localbackup/attributes/default.rb
@@ -6,13 +6,10 @@ set[:localbackup][:script_file] = "#{script_dir}/make_backup.sh"
 default[:localbackup][:destination] = '/usr/local/backups'
 default[:localbackup][:dirs] = %w{ /etc }
 default[:localbackup][:ignore] = []
+default[:localbackup][:tar_options] = '--warning=no-file-changed --warning=no-file-removed'
 
 default[:localbackup][:mysql][:enable] = File.exists?('/etc/init.d/mysql')
 default[:localbackup][:pgsql][:enable] = (not Dir.glob('/etc/init.d/postgresql*').empty?)
-
-# MySQL 5.1 an up needs `--events`
-# MySQL 5.0 and down doesn't have it
-default[:localbackup][:mysql][:extra_options] = '--events'
 
 case node[:platform]
 when 'ubuntu'
@@ -20,7 +17,11 @@ when 'ubuntu'
         /etc
         /var/backups
     }
-    if node[:platform_version].to_f < 10.04
-        default[:localbackup][:mysql][:extra_options] = ''
+    if node[:platform_version].to_f < 12.04
+        default[:localbackup][:tar_options] = ''
+    end
+when 'redhat'
+    if node[:platform_version].to_f < 6
+        default[:localbackup][:tar_options] = ''
     end
 end
