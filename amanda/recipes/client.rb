bbb7b1236281fb07765678f9bc592ed43456dd31
Hoang Xuan Phu <phunehehe@gmail.com>
2013-07-10 08:38:04 +0000

    support ubuntu in general

--- amanda/recipes/client.rb
+++ amanda/recipes/client.rb
@@ -11,16 +11,18 @@ amanda = node[:amanda]
 arch = node[:kernel][:machine] =~ /x86_64/ ? 'amd64' : 'i386'
 
 
-if node[:platform] == 'ubuntu' and node['platform_version'].to_f == 12.04
-  amanda_pkg = "amanda-backup-client_#{amanda[:version]}-1Ubuntu1204_#{arch}.deb"
-  pkg_file = "#{Chef::Config[:file_cache_path]}/#{amanda_pkg}"
+if node[:platform] == 'ubuntu'
+  version_without_dot = node[:platform_version].to_s.delete '.'
+  amanda_pkg = "amanda-backup-client_#{amanda[:version]}-1Ubuntu#{version_without_dot}_#{arch}.deb"
+  package_url = "http://www.zmanda.com/downloads/community/Amanda/#{amanda[:version]}/Ubuntu-#{node[:platform_version]}/#{amanda_pkg}"
 else
   raise NotImplementedError
 end
 
+pkg_file = "#{Chef::Config[:file_cache_path]}/#{amanda_pkg}"
 
 remote_file pkg_file do
-  source "http://www.zmanda.com/downloads/community/Amanda/#{amanda[:version]}/Ubuntu-12.04/#{amanda_pkg}"
+  source package_url
   action :create_if_missing
 end
 
