87b1701469b0411e61d22b31413b1f0216bd21ef
Hoang Xuan Phu <phunehehe@gmail.com>
2013-08-22 17:15:25 +0200

    use check-updates from epel

--- nagios/recipes/redhat.rb
+++ nagios/recipes/redhat.rb
@@ -10,7 +10,7 @@
 include_recipe 'yum::epel'
 
 
-%w{ nagios-nrpe nagios-plugins-all }.each do |pkg|
+%w{ nagios-nrpe nagios-plugins-all nagios-plugins-check-updates }.each do |pkg|
     package pkg do
         action :install
     end
@@ -22,12 +22,4 @@ plugin_dir = node[:nagios][:plugin_dir]
 directory plugin_dir do
     action :create
     recursive true
-end
-
-
-%w{ check_updates }.each do |plugin|
-    template "#{plugin_dir}/#{plugin}" do
-        source "redhat-#{plugin}.erb"
-        mode '0775'
-    end
 end
