3d190c405d591628f942d322e81a153f4e085cf8
Hoang Xuan Phu <phunehehe@gmail.com>
2013-08-23 14:31:23 +0700

    import https://github.com/elasticsearch/cookbook-elasticsearch/tree/4ada6e4736aa54065774e92fa07d4b4de7bb76d3

---
+++ elasticsearch/metadata.rb
@@ -0,0 +1,25 @@
+name             "elasticsearch"
+
+maintainer       "karmi"
+maintainer_email "karmi@karmi.cz"
+license          "Apache"
+description      "Installs and configures elasticsearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version          "0.3.3"
+
+depends 'ark'
+
+recommends 'build-essential'
+recommends 'xml'
+recommends 'java'
+recommends 'monit'
+
+provides 'elasticsearch'
+provides 'elasticsearch::data'
+provides 'elasticsearch::ebs'
+provides 'elasticsearch::aws'
+provides 'elasticsearch::nginx'
+provides 'elasticsearch::proxy'
+provides 'elasticsearch::plugins'
+provides 'elasticsearch::monit'
+provides 'elasticsearch::search_discovery'
