5e4b71d886fb0a0872e87fbf9b8deb9fea9811e4
Hoang Xuan Phu <phunehehe@gmail.com>
2013-09-05 14:15:22 +0700

    udpate nodejs, rid of npm

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -5,6 +5,5 @@ description      "Installs/Configures nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.6.11"
 recipe           "nodejs", "Installs Node.JS from source"
-recipe           "nodejs::npm", "Installs npm - a package manager for node"
 
 depends          "build-essential"
