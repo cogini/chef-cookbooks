e0f61f2014ec96cff813ef236b19bbf02c5941bd
Nhan Nguyen <ducnhan813@gmail.com>
2013-09-11 16:18:47 +0700

    add chili

---
+++ chili/metadata.rb
@@ -0,0 +1,11 @@
+maintainer          "Cogini"
+maintainer_email    "nhan@cogini.com"
+license             "All rights reserved"
+description         "Install/Configure redmine"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "0.0.1"
+
+depends             "apt"
+depends             "git"
+depends             "postgresql"
+depends             "nginx"
