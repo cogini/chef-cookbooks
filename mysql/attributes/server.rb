86913f441c2367e95078475a3894ff3d96780f1d
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-10-01 04:55:25 +0000

    use default location for slow log

--- mysql/attributes/server.rb
+++ mysql/attributes/server.rb
@@ -112,7 +112,7 @@ default['mysql']['tunable']['wait_timeout']         = "180"
 default['mysql']['tunable']['query_cache_limit']    = "1M"
 default['mysql']['tunable']['query_cache_size']     = "16M"
 
-default['mysql']['tunable']['log_slow_queries']     = "/var/log/mysql/slow.log"
+default['mysql']['tunable']['log_slow_queries']     = '/var/log/mysql/mysql-slow.log'
 default['mysql']['tunable']['long_query_time']      = 2
 
 default['mysql']['tunable']['expire_logs_days']     = 10
