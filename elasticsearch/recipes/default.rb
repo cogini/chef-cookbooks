43ebf71bcc03df2734872e43e1324215ec391b02
Nhan Nguyen <ducnhan813@gmail.com>
2013-10-15 10:39:21 +0700

    update elasticsearch to 0.9.5

--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -1,2 +1,4 @@
 include_recipe 'java'
-package_from_url 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb'
+
+
+package_from_url node[:elasticsearch][:download_url]
