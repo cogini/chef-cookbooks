8ca4e452355a772081b59e5f16e95e20d426cd4b
Nhan Nguyen <ducnhan813@gmail.com>
2013-10-17 18:19:01 +0700

    clone a git repo as a specified user and checkout a specified branch/tag

--- basics/definitions/git_clone.rb
+++ basics/definitions/git_clone.rb
@@ -1,10 +1,27 @@
-define :git_clone, :destination => nil do
+define :git_clone, :destination => nil, :version => nil do
 
-  url = params[:name]
-  destination = params[:destination]
+    url = params[:name]
+    destination = params[:destination]
+    user = if params[:user]
+              params[:user]
+           else
+              "root"
+           end
 
-  execute "git clone #{url} #{destination}" do
-    not_if { Dir.exists?(destination) }
-  end
+    execute "git clone #{url} #{destination}" do
+        user user
+        not_if { File.exists?(destination) }
+    end
+
+    if params[:version]
+        bash "Checkout #{params[:version]}" do
+            user user
+            code <<-EOH
+                cd #{destination}
+                git fetch
+                git checkout -f #{version}
+            EOH
+        end
+    end
 
 end
