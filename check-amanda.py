dafe5c0439edc9e03415c870f3e0f252502d3a59
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-10-21 15:29:13 +0000

    refactor

--- check-amanda.py
+++ check-amanda.py
@@ -14,26 +14,14 @@ def enter_line(stream, line):
     stream.write(line + '\n')
 
 
-def write_dirs_to_pipe(dirs, stdin):
-    try:
-        while True:
-            dir = dirs.get(True, 1)
-            enter_line(stdin, 'cd %s' % dir)
-            enter_line(stdin, 'ls')
-    except Empty:
-        #print 'No more dir to write'
-        pass
-
-
 def read_pipe_to_lines(stdout, lines):
     while True:
         line = stdout.readline()
         lines.put(line)
 
 
-def process_lines(disk, lines, dirs, files):
+def process_lines(lines, files, stdin):
 
-    dir_path = disk
     pattern = re.compile('\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}')
 
     try:
@@ -58,7 +46,8 @@ def process_lines(disk, lines, dirs, files):
 
             abs_path = path.join(dir_path, the_path)
             if abs_path.endswith(path.sep):
-                dirs.put(abs_path)
+                enter_line(stdin, 'cd %s' % abs_path)
+                enter_line(stdin, 'ls')
             else:
                 #print 'Adding', abs_path
                 files.append(abs_path)
@@ -70,7 +59,6 @@ def process_lines(disk, lines, dirs, files):
 
 def get_file_list(config, hostname, disk):
 
-    dirs = Queue()
     files = []
     lines = Queue()
 
@@ -80,80 +68,82 @@ def get_file_list(config, hostname, disk):
 
     enter_line(stdin, 'sethost %s' % hostname)
     enter_line(stdin, 'setdisk %s' % disk)
+    enter_line(stdin, 'cd %s' % disk)
     enter_line(stdin, 'ls')
-
-    write_thread = Thread(target=write_dirs_to_pipe, args=(dirs, stdin))
-    write_thread.daemon = True
-    write_thread.start()
 
     read_thread = Thread(target=read_pipe_to_lines, args=(stdout, lines))
     read_thread.daemon = True
     read_thread.start()
 
-    process_thread = Thread(target=process_lines, args=(disk, lines, dirs, files))
+    process_thread = Thread(target=process_lines, args=(lines, files, stdin))
     process_thread.daemon = True
     process_thread.start()
 
-    write_thread.join()
     process_thread.join()
     return files
 
 
-prefix = '/tmp/check-amanda'
-try:
-    makedirs(prefix)
-except OSError:
-    print 'Directory %s already exists.' % prefix
+def test_extraction(config, hostname, disk, target):
+
+    prefix = '/tmp/check-amanda'
+    try:
+        makedirs(prefix)
+    except OSError:
+        print 'Directory %s already exists.' % prefix
+
+    output_path = path.join(prefix, target)
+
+    p = Popen(['amrecover', config], stdin=PIPE, stdout=PIPE)
+    stdin = p.stdin
+
+    enter_line(stdin, 'lcd %s' % prefix)
+    enter_line(stdin, 'sethost %s' % hostname)
+    enter_line(stdin, 'setdisk %s' % disk)
+    enter_line(stdin, 'add %s' % target)
+    enter_line(stdin, 'extract')
+    enter_line(stdin, 'Y')
+    enter_line(stdin, 'Y')
+    enter_line(stdin, 'exit')
+    p.communicate()
+
+    p = Popen(['file', output_path], stdin=PIPE, stdout=PIPE)
+    output, error = p.communicate()
+    print output.strip()
+    assert error is None
+    assert p.returncode == 0
+
+    if not path.islink(output_path):
+        size = path.getsize(output_path)
+        assert size > 0
+        print 'File size is %s.' % size
+
+    rmtree(prefix)
 
 
-dna = json.load(open('/etc/chef/node.json'))
-locations = dna['amanda']['backup_locations']
+def main():
 
-hosts = [host['hostname'] for host in locations]
-hostname = choice(hosts)
+    dna = json.load(open('/etc/chef/node.json'))
+    locations = dna['amanda']['backup_locations']
 
-disks = [host['locations'] for host in locations if host['hostname'] == hostname][0]
-disk = choice(disks)
+    hosts = [host['hostname'] for host in locations]
+    hostname = choice(hosts)
 
-config = choice(('daily', 'weekly', 'monthly'))
+    disks = [host['locations'] for host in locations if host['hostname'] == hostname][0]
+    disk = choice(disks)
 
-print 'Checking %s backup of %s:%s ...' % (config, hostname, disk)
+    config = choice(('daily', 'weekly', 'monthly'))
+    print 'Checking %s backup of %s:%s ...' % (config, hostname, disk)
 
+    files = get_file_list(config, hostname, disk)
+    print 'There are %s files.' % len(files)
 
-files = get_file_list(config, hostname, disk)
-print 'There are %s files.' % len(files)
+    random_file = choice(files)
 
+    print 'Trying to extract %s ...' % random_file
+    test_extraction(config, hostname, disk, random_file[len(disk) + 1:])
 
-random_file = choice(files)
-output_path = path.join(prefix, random_file[len(disk) + 1:])
-print 'Trying to extract %s ...' % random_file
-
-p = Popen(['amrecover', config], stdin=PIPE, stdout=PIPE)
-stdin = p.stdin
-
-enter_line(stdin, 'lcd %s' % prefix)
-enter_line(stdin, 'sethost %s' % hostname)
-enter_line(stdin, 'setdisk %s' % disk)
-enter_line(stdin, 'add %s' % random_file)
-enter_line(stdin, 'extract')
-enter_line(stdin, 'Y')
-enter_line(stdin, 'Y')
-enter_line(stdin, 'exit')
-p.communicate()
+    print 'Everything looks OK.'
 
 
-p = Popen(['file', output_path], stdin=PIPE, stdout=PIPE)
-output, error = p.communicate()
-print output.strip()
-assert error is None
-assert p.returncode == 0
-
-
-if not path.islink(output_path):
-    size = path.getsize(output_path)
-    assert size > 0
-    print 'File size is %s.' % size
-
-
-rmtree(prefix)
-print 'Everything looks OK.'
+if __name__ == '__main__':
+    main()
