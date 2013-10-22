acfc84a2e6820ae240a4e75e3e9311fc55d664ad
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-10-22 06:06:17 +0000

    add dependency

--- erlang/metadata.rb
+++ erlang/metadata.rb
@@ -5,5 +5,6 @@ description         "Installs/configures erlang"
 long_description    IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version             "0.0.1"
 
+depends             "basics"
 depends             "fpm"
 depends             "git"
