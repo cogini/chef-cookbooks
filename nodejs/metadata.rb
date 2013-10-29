dfa2eec20dc1377770171412b468e47aac3511a7
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-10-29 16:00:32 +0700

    nodejs use packages.cogini.com

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -1,10 +1,8 @@
-maintainer       "Promet Solutions"
-maintainer_email "marius@promethost.com"
-license          "Apache 2.0"
-description      "Installs/Configures nodejs"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.6.11"
-recipe           "nodejs", "Installs Node.JS from source"
-
-depends          "build-essential"
-depends          "fpm"
+name             'nodejs'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures nodejs'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'basics'
