45bc683b70afde82e4e429e9ee0191b90b582b6e
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-11-04 09:51:25 +0000

    set allocated memory for elasticsearch

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -37,11 +37,7 @@ default.elasticsearch[:pid_file]  = "#{node.elasticsearch[:pid_path]}/#{node.ela
 
 # === MEMORY
 #
-# Maximum amount of memory to use is automatically computed as one half of total available memory on the machine.
-# You may choose to set it in your node/role configuration instead.
-#
-allocated_memory = "#{(node.memory.total.to_i * 0.6 ).floor / 1024}m"
-default.elasticsearch[:allocated_memory] = allocated_memory
+default[:elasticsearch][:allocated_memory] = nil
 
 # === LIMITS
 #
--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -2,3 +2,11 @@ include_recipe 'java'
 
 
 package_from_url node[:elasticsearch][:download_url]
+
+
+if node[:platform_family] == 'rhel'
+    template '/etc/sysconfig/elasticsearch' do
+        source 'redhat-sysconfig.erb'
+        mode '644'
+    end
+end
