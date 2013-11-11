8dfa01403abd16907308b18c4308ade0d1043eb4
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-11-11 17:36:23 +0700

    remove /var/backups

--- tarsnap/attributes/default.rb
+++ tarsnap/attributes/default.rb
@@ -1,16 +1,5 @@
-default[:tarsnap][:version] = '1.0.33'
-default[:tarsnap][:script_dir] = '/root/tarsnap'
-default[:tarsnap][:ignore_file] = '/root/tarsnap/ignore'
+default[:tarsnap][:dirs] = %w{ /etc }
 default[:tarsnap][:ignore] = []
-
-case node[:platform]
-when 'ubuntu'
-    default[:tarsnap][:dirs] = %w{
-        /etc
-        /var/backups
-    }
-else
-    default[:tarsnap][:dirs] = %w{
-        /etc
-    }
-end
+default[:tarsnap][:ignore_file] = '/root/tarsnap/ignore'
+default[:tarsnap][:script_dir] = '/root/tarsnap'
+default[:tarsnap][:version] = '1.0.33'
