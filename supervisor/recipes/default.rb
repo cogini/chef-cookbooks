098e2d1ce78005b8e8792377b38937c4b8ea7975
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-11-13 14:30:05 +0700

    use package_from_url for supervisor

--- supervisor/recipes/default.rb
+++ supervisor/recipes/default.rb
@@ -7,44 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "fpm"
-include_recipe "python"
+package_from_url "http://packages.cogini.com/supervisor-#{node[:supervisor][:version]}-noarch.rpm"
 
-supervisor_version = node[:supervisor][:version]
-chef_cache = Chef::Config[:file_cache_path]
-pkg_file = "#{chef_cache}/python-supervisor-#{supervisor_version}.rpm"
-
-unless File.exists?(pkg_file)
-    bash "build supervisor" do
-        cwd chef_cache
-        code <<-EOH
-            path_extras="/var/lib/gems/1.8/bin"
-            for extra in $(echo $path_extras)
-                do
-                test -d $extra && test ":$PATH:" != "*:$extra:*" && PATH="$extra:$PATH"
-            done
-            fpm -s python -t rpm -p #{pkg_file} -v #{supervisor_version} supervisor
-        EOH
-    end
+template '/etc/init.d/supervisord' do
+    mode '755'
 end
 
-package "python-supervisor" do
-    source pkg_file
-    options "--nogpgcheck"
-end
-
-template "/etc/init.d/supervisord" do
-    source "_etc_init.d_supervisord.erb"
-    mode "0755"
-end
-
-service "supervisord" do
+service 'supervisord' do
     supports :status => true, :restart => true, :reload => true
     action [ :enable, :start ]
 end
 
 template '/etc/supervisord.conf' do
-    source '_etc_supervisord.conf.erb'
     mode '644'
     notifies :reload, 'service[supervisord]'
 end
