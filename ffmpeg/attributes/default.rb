ff8634511922d33ae05318577791898d0996442b
Nhan Nguyen <ducnhan813@gmail.com>
2013-11-22 23:37:11 +0700

    ffmpeg 64bit and 32bit

--- ffmpeg/attributes/default.rb
+++ ffmpeg/attributes/default.rb
@@ -1,2 +1,3 @@
 default[:ffmpeg][:version] = '0.10.3'
-default[:ffmpeg][:package_url] = "http://packages.cogini.com/ffmpeg-#{node[:ffmpeg][:version]}.rpm"
+arch = node[:kernel][:machine] =~ /x86_64/ ? '64bit' : '32bit'
+default[:ffmpeg][:package_url] = "http://packages.cogini.com/ffmpeg-#{node[:ffmpeg][:version]}-#{arch}.rpm"
