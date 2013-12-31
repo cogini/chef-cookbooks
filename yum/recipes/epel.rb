57c9897e13489c7fd956aff3582fc5d1b02ab48f
Hoang Xuan Phu <phuhimself@phunehehe.net>
2013-12-31 16:36:35 +0700

    import https://github.com/opscode-cookbooks/yum/tree/277d11028be918f4e1ca40d201f14e17bed0ecc3

--- yum/recipes/epel.rb
+++ yum/recipes/epel.rb
@@ -5,24 +5,8 @@
 # Copyright 2012, Cogini
 #
 
-key = "#{node.platform}#{node.platform_version.to_i}"
-
-case key
-when 'centos6', 'amazon2012'
-    url = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
-when 'centos5'
-    url = 'http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm'
-else
-    raise NotImplementedError
-end
-
-rpm_file = "#{Chef::Config[:file_cache_path]}/#{File.basename(url)}"
-
-remote_file rpm_file do
-    source url
-    action :create_if_missing
-end
-
-package 'epel-release' do
-    source rpm_file
+yum_repository 'epel' do
+    description 'Extra Packages for Enterprise Linux 6 - $basearch'
+    mirrorlist 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch'
+    gpgkey 'https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
 end
