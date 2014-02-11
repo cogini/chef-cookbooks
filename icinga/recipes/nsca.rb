a24df89d14375762794cd5a39e9f0058c8e0155c
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-02-11 09:57:11 +0000

    add template for nsca.cfg

--- icinga/recipes/nsca.rb
+++ icinga/recipes/nsca.rb
@@ -3,3 +3,12 @@ unless node[:icinga][:check_external_commands] == 1
 end
 
 package 'nsca'
+
+service 'nsca' do
+    action [:enable, :start]
+end
+
+template '/etc/nsca.cfg' do
+    mode '600'
+    notifies :restart, 'service[nsca]'
+end
