22370768cbdb7bef90f0f7bafef77fc823d83047
Nhan Nguyen <ducnhan813@gmail.com>
2014-03-03 14:56:46 +0700

    add git to list of dependencies

--- yii/metadata.rb
+++ yii/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures yii"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends          "git"
