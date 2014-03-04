45a2d4d61e88967c25429f694c57bc636b7f12d3
nhanb <thanhnhan483@gmail.com>
2014-03-04 10:10:07 +0700

    Refactor definition into resource/provider pair
    - Usage stays the same (calling yii_framework block)
    - Remove unnecessary files (attribute, definition, library)
    - Add check if path to symlink exists. Try to mkdir if not.

--- yii/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:yii][:version] = '1.1.10'
-default[:yii][:path] = '/opt/yii'
-default[:yii][:symlink] = nil
---
+++ yii/providers/framework.rb
@@ -0,0 +1,34 @@
+use_inline_resources
+
+action :create do
+
+    include_recipe 'git'
+
+    version = new_resource.version
+    path = new_resource.path || "/opt/yii-#{version}"
+    symlink = new_resource.symlink
+
+    directory ::File::dirname(path) do
+        action :create
+        recursive true
+    end
+
+    git "Clone yii #{version}" do
+        repository "https://github.com/yiisoft/yii.git"
+        destination path
+        reference version
+    end
+
+    if symlink then
+
+        # make sure path to symlink exists
+        directory ::File::dirname(symlink) do
+            action :create
+            recursive true
+        end
+
+        link symlink do
+            to path
+        end
+    end
+end
