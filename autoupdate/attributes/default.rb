d5ab67bd59f447c95f2eb59d895e3fa57cfff89e
Nhan Nguyen <ducnhan813@gmail.com>
2014-03-04 18:28:47 +0700

    add autoupdate for centos/redhat

--- autoupdate/attributes/default.rb
+++ autoupdate/attributes/default.rb
@@ -6,13 +6,18 @@ when 'ubuntu'
         "updates"
     ]
     default[:autoupdate][:auto_fix_interrupted_dpkg] = true
-    default[:autoupdate][:email] = "noc@cogini.com"
-    deafult[:autoupdate][:remove_unused_dependencies] = true
-    deafult[:autoupdate][:update_package_lists_interval] = 1
     default[:autoupdate][:clean_downloaded_packages_interval] = 15
+    default[:autoupdate][:email] = "noc@cogini.com"
+    default[:autoupdate][:remove_unused_dependencies] = true
+    default[:autoupdate][:update_package_lists_interval] = 1
     default[:autoupdate][:upgrade_interval] = 1
 
 when 'centos', 'redhat'
+    default[:autoupdate][:check_first] = false
+    default[:autoupdate][:check_only] = false
+    default[:autoupdate][:download_only] = false
 else
     raise NotImplementedError
 end
+
+default[:autoupdate][:email] = "admin@example.com"
