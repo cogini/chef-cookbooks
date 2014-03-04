24a6c6042dec8c38bc8c742ed4d03d3dbae8bdc7
Nhan Nguyen <ducnhan813@gmail.com>
2014-03-04 18:30:27 +0700

    add dependency

--- autoupdate/metadata.rb
+++ autoupdate/metadata.rb
@@ -4,3 +4,5 @@ maintainer_email 'nhan@cogini.com'
 license          'All rights reserved'
 description      'Enable autoupdate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+depends          'apt'
