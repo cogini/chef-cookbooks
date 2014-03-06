f15133c50d10500e5ad19a2f9283a292aa86e46a
Nhan Nguyen <ducnhan813@gmail.com>
2014-03-06 10:22:12 +0700

    install postgresql base on version in repo

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -42,5 +42,5 @@ template "#{node[:postgresql][:dir]}/pg_hba.conf" do
   owner "postgres"
   group "postgres"
   mode 0600
-  notifies :reload, 'service[postgresql]', :immediately
+  notifies :reload, "service[postgresql-#{node[:postgresql][:version]}]", :immediately
 end
