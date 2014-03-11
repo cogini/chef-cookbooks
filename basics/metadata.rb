1cd6fcda8cebd47fd0b18ab5eddba95c4591a8d0
Nhan Nguyen <ducnhan813@gmail.com>
2014-03-11 11:12:16 +0700

    add autoupdate dependency to basics

--- basics/metadata.rb
+++ basics/metadata.rb
@@ -5,6 +5,7 @@ description      "Installs/Configures basics"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "apt"
+depends          "autoupdate"
 depends          "gdebi"
 depends          "localbackup"
 depends          "postfix"
