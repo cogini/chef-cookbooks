b75ef0a65951d1713c9af5d2077e73f1fea20f91
Hoang Xuan Phu <phunehehe@gmail.com>
2014-04-04 04:56:23 +0000

    only add repos when needed

--- postgresql/recipes/client_redhat.rb
+++ postgresql/recipes/client_redhat.rb
@@ -1,13 +1,12 @@
 version = node[:postgresql][:version]
 
-yum_repository "pgdg-#{version}" do
-    baseurl "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
-    description "PostgreSQL #{version}"
-    gpgkey 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
-end
-
-if node[:postgresql][:version] == node[:postgresql][:repo_version]
+if version == node[:postgresql][:repo_version]
     package 'postgresql-devel'
 else
-    package "postgresql#{node[:postgresql][:version].split('.').join}-devel"
+    yum_repository "pgdg-#{version}" do
+        baseurl "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
+        description "PostgreSQL #{version}"
+        gpgkey 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
+    end
+    package "postgresql#{version.split('.').join}-devel"
 end
