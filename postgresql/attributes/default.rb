788b1c24d1e7c3c164550c303529f3a2a8648330
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-04-10 08:29:57 +0000

    add an attribute for wal_keep_segments

--- postgresql/attributes/default.rb
+++ postgresql/attributes/default.rb
@@ -94,6 +94,7 @@ default[:postgresql][:config][:hot_standby] = "off"
 default[:postgresql][:config][:log_min_duration_statement] = 250
 default[:postgresql][:config][:max_locks_per_transaction] = 64
 default[:postgresql][:config][:max_pred_locks_per_transaction] = 64
+default[:postgresql][:config][:wal_keep_segments] = 0
 
 default[:postgresql][:master_host] = nil
 set[:postgresql][:is_slave] = node[:postgresql][:master_host]
