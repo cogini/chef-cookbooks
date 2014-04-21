c240547477a22e52ccbd8e4ba22b5c57662f63f2
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-04-21 14:14:49 +0700

    add note about update-alternatives

--- postgresql/recipes/client.rb
+++ postgresql/recipes/client.rb
@@ -5,6 +5,10 @@ when 'debian'
     include_recipe 'postgresql::client_debian'
 end
 
+# TODO HXP: This is only needed when using pgdg. However, packages in pgdg use
+# update-alternatives so the right way is `sudo update-alternatives --set
+# pgsql-pg_dumpall /usr/pgsql-9.2/bin/pg_dumpall`. Also, for some reason
+# pg_config is not managed by update-alternatives.
 link '/usr/bin/pg_config' do
     to "/usr/pgsql-#{node[:postgresql][:version]}/bin/pg_config"
     not_if { File.exist?('/usr/bin/pg_config') }
