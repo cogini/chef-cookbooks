f0f69ec043ffe61a749faf4b379c5f10490e4249
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-04-23 08:42:45 +0000

    verify ssl

--- solo.rb
+++ solo.rb
@@ -1,3 +1,6 @@
+# This should be the default, but until then this will do.
+ssl_verify_mode :verify_peer
+
 chef_dir = File.expand_path(File.dirname(__FILE__))
 cookbook_path [
     "#{chef_dir}/chef-cookbooks",
