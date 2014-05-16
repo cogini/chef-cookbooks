1256c520374eb8bb6af9f36b848f414001c553cc
Nhan Nguyen <ducnhan813@gmail.com>
2014-05-16 12:11:55 +0700

    remove unneeded files

--- vsftpd/recipes/default.rb
+++ vsftpd/recipes/default.rb
@@ -14,39 +14,13 @@ service "vsftpd" do
   action [ :enable, :start ]
 end
 
-if node[:vsftpd][:use_ssl_certs_from_cookbook]
-  cookbook_file "#{node[:vsftpd][:ssl_cert_path]}/#{node[:vsftpd][:ssl_certs_basename]}.pem" do
-    owner 'root'
-    group 'root'
-    mode 0600
-  end
-
-  cookbook_file "#{node[:vsftpd][:ssl_private_key_path]}/#{node[:vsftpd][:ssl_certs_basename]}.key" do
-    owner 'root'
-    group 'root'
-    mode 0600
-  end
-end
 
 template "/etc/vsftpd.chroot_list" do
-  source "vsftpd.chroot_list"
-  owner "root"
-  group "root"
   mode 0644
-  variables(:users => node[:vsftpd][:chroot_users])
 end
 
 template "/etc/vsftpd.conf" do
-  source "vsftpd.conf"
-  owner "root"
-  group "root"
   mode 0644
-  variables(
-    :ssl_cert_path => node[:vsftpd][:ssl_cert_path],
-    :ssl_private_key_path => node[:vsftpd][:ssl_private_key_path],
-    :ssl_certs_basename => node[:vsftpd][:ssl_certs_basename],
-    :chroot_local_user => node[:vsftpd][:chroot_local_user]
-  )
-  notifies :restart, resources(:service => "vsftpd")
+  notifies :restart, 'service[vsftpd]', :delayed
 end
 
