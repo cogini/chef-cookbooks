b9239e76c135eaed3911b0d16e9d54f75fce3202
nhanb <thanhnhan483@gmail.com>
2014-06-09 13:52:04 +0700

    add PHP opcache module recipe

---
+++ php/recipes/module_opcache.rb
@@ -0,0 +1,13 @@
+php_pear "zendopcache" do
+  action :install
+  preferred_state :beta  # for now there is no "stable" zendopcache package
+  zend_extensions ['opcache.so']
+  directives(
+    :memory_consumption => 128,
+    :interned_strings_buffer => 8,
+    :max_accelerated_files => 4000,
+    :revalidate_freq => 60,
+    :fast_shutdown => 1,
+    :enable_cli => 1,
+  )
+end
