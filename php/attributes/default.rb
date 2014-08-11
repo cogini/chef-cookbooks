9b3944a27a90e1ee3452a8e59303f2d9998523f8
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-08-11 05:58:28 +0000

    fix opcache defaults

--- php/attributes/default.rb
+++ php/attributes/default.rb
@@ -105,9 +105,9 @@ default[:php][:configure_options] = %W{--prefix=#{php[:prefix_dir]}
                                           --with-pdo-mysql
                                           --with-pdo-sqlite}
 
-node[:php][:opcache][:memory_consumption] = 128
-node[:php][:opcache][:interned_strings_buffer] = 8
-node[:php][:opcache][:max_accelerated_files] = 4000
-node[:php][:opcache][:revalidate_freq] = 60
-node[:php][:opcache][:fast_shutdown] = 1
-node[:php][:opcache][:enable_cli] = 1
+default[:php][:opcache][:memory_consumption] = 128
+default[:php][:opcache][:interned_strings_buffer] = 8
+default[:php][:opcache][:max_accelerated_files] = 4000
+default[:php][:opcache][:revalidate_freq] = 60
+default[:php][:opcache][:fast_shutdown] = 1
+default[:php][:opcache][:enable_cli] = 1
--- php/recipes/module_opcache.rb
+++ php/recipes/module_opcache.rb
@@ -2,12 +2,5 @@ php_pear "zendopcache" do
   action :install
   preferred_state :beta  # for now there is no "stable" zendopcache package
   zend_extensions ['opcache.so']
-  directives(
-    :memory_consumption => 128,
-    :interned_strings_buffer => 8,
-    :max_accelerated_files => 4000,
-    :revalidate_freq => 60,
-    :fast_shutdown => 1,
-    :enable_cli => 1,
-  )
+  directives node[:php][:opcache]
 end
