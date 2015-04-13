1982cd46dd6fd52f27d7ce239283a56f2950e908
Nhan Nguyen <ducnhan813@gmail.com>
2015-04-13 10:00:58 +0700

    add missing metadata

--- collectd/metadata.rb
+++ collectd/metadata.rb
@@ -1,3 +1,9 @@
-name    'collectd'
+name                'collectd'
+maintainer          "Cogini"
+maintainer_email    "nhan@cogini.com"
+license             "All rights reserved"
+description         "Install/Configure redmine"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "0.0.1"
 depends 'basics'
 depends 'nginx'
--- owncloud/metadata.rb
+++ owncloud/metadata.rb
@@ -1,4 +1,10 @@
 name    'owncloud'
+maintainer       'Cogini'
+maintainer_email 'nhan@cogini.com'
+license          'All rights reserved'
+description      'Installs/Configures gitlab'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
 depends 'apt'
 depends 'git'
 depends 'nginx'
